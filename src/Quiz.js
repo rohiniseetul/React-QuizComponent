b3d35af1541d925cc020bdc05ea3b5fd959ee44b
Rohini Seetul <rohini.seetul@fenergo.com>
2019-05-27 14:11:26 +0100

    Resetting the quiz - module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -19,6 +19,12 @@ class Quiz extends Component {
     }));
   }
 
+  handleResetClick() {
+    this.setState({
+      quiz_position: 1
+    });
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -26,7 +32,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             quiz_question={
