d67baa80a8c1608b49e5f10ed8583a1016f3d183
Rohini Seetul <rohini.seetul@fenergo.com>
2019-05-27 13:51:23 +0100

    Creating QuizEnd component - module 4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import QuizQuestion from "./QuizQuestion.js";
+import QuizEnd from "./QuizEnd.js";
 
 const quizData = require("./quiz_data.json");
 
@@ -13,13 +14,20 @@ class Quiz extends Component {
   }
 
   render() {
-    const { quiz_position } = this.state;
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
     return (
       <div>
-        <QuizQuestion
-          quiz_question={quizData.quiz_questions[quiz_position - 1]}
-        />
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
       </div>
     );
   }
